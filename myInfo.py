919bdd6207b83d939ad95c37c3888e78d6025395
KevinBotwinick <91150801+KevinBotwinick@users.noreply.github.com>
2022-01-26 22:26:05 -0500

    myInfo.py

--- myInfo.py
+++ myInfo.py
@@ -1,6 +1,10 @@
 #CS175-01
 #Kevin Botwinick
 #myInfo
-junk = "I am a teacher"
-print("Hello my name is Kevin Botwinick")
-print(junk)
+name = "I am Kevin Botwinick"
+print("Hello my name is Kevin Botwinick and this is my info")
+address = '26 Elmo Drive Old Bridge New Jersey 08857'
+print("26 Elmo Drive Old Bridge New Jersey 08857")
+telephone = '732 778 6672'
+print("732 778 6672")
+print("My major is computer science")
