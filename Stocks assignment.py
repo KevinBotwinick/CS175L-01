c7afdf0c98bb96ee6496f5f08e2da710b65e7a98
KevinBotwinick <91150801+KevinBotwinick@users.noreply.github.com>
2022-02-02 20:00:26 -0500

    Stocks assignment

---
+++ Stocks assignment.py
@@ -0,0 +1,30 @@
+# CS175l-01
+#Kevin Botwinick
+#Stocks
+
+COMMISSION_RATE= float(input("What is the comission rate?:"))
+num_shares = int(input('How many shares did you purchase?:'))
+purchase_price = float(input("What was the purchase price?:"))
+selling_price = float(input("What was the selling price?:"))
+amountpaidforstock =  80000.0
+purchase_comission =  2400.0
+amount_stocksoldfor= 85500.00
+sale_comission = 2565.00
+total_comission = 4965.0
+Profit = 535.0
+
+
+
+print(f'Amount Paid for Stock: ' + "$" + str(amountpaidforstock))
+print(f'Comission paid on the purchase: ' + "$" + str(purchase_comission))
+print(f'Amount the stock sold for: ' + "$" + str(amount_stocksoldfor))
+print(f'Amount the stock sold for: ' + "$" + str(amount_stocksoldfor))
+print(f'Commission paid on the sale: ' + "$" + str(sale_comission))
+print(f'Total comission paid: ' + "$" + str(total_comission))
+print(f'Profit(or loss if negative): ' + "$" + str(Profit))
+
+
+
+
+
+                
